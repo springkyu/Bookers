ef87decfe4e4a0652b2d27a58bf5a648b561f7d4
springkyu <seika.k.r0610@gmail.com>
2021-07-14 06:44:54 +0000

    [ADD]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.all
   end
 
   def show
+    @book = Book.find(books_params)
   end
 
   def new
-    
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book(params[:id])
   end
 
   def edit
   end
+
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
+
 end
