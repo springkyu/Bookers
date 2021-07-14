36a361d36f6894f9e7d334fc8812537a748f7387
springkyu <seika.k.r0610@gmail.com>
2021-07-14 09:22:53 +0000

    [UPDATE]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(params[:id])
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -20,9 +20,10 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update
-    redirect_to book_path(book)
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
+
 
   def destroy
     book = Book.find(params[:id])
