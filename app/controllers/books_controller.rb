7427ba0f922fcf720d529c1b864f9b2b610a753d
springkyu <seika.k.r0610@gmail.com>
2021-07-14 13:00:16 +0000

    [update]/基盤3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,12 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    if book.update(book_params)
-      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
       flash.now[:danger] = 'failed.'
+      render :edit
     end
   end
 
